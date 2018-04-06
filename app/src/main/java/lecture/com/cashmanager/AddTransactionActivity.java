a7411fb3357f6d8daddfb4c24bc9d2c390e231b8
duonghan <20155296@student.hust.edu.vn>
2018-04-06 21:30:18 +0700

    Add new Activity - Transaction Activity

---
+++ app/src/main/java/lecture/com/cashmanager/AddTransactionActivity.java
@@ -0,0 +1,13 @@
+package lecture.com.cashmanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddTransactionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_transaction);
+    }
+}
