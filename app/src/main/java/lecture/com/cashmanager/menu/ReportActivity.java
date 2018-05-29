fa09099f869bb529928d7b0e08c85dd6b92cc9fc
duonghan <20155296@student.hust.edu.vn>
2018-05-29 16:47:33 +0700

    update report demo test layout

--- app/src/main/java/lecture/com/cashmanager/menu/ReportActivity.java
+++ app/src/main/java/lecture/com/cashmanager/menu/ReportActivity.java
@@ -11,9 +11,12 @@ import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.xml.transform.Templates;
 
 import lecture.com.cashmanager.R;
 
@@ -32,16 +35,26 @@ public class ReportActivity extends AppCompatActivity {
         barChart = (BarChart) findViewById(R.id.chart_bar);
         pieChart = (PieChart) findViewById(R.id.chart_pie);
 
-        List<BarEntry> entriesBar = new ArrayList<>();
-        entriesBar.add(new BarEntry(0f, 30f));
-        entriesBar.add(new BarEntry(1f, 80f));
-        entriesBar.add(new BarEntry(2f, 60f));
-        entriesBar.add(new BarEntry(3f, 50f));
-        // gap of 2f
-        entriesBar.add(new BarEntry(5f, 70f));
-        entriesBar.add(new BarEntry(6f, 60f));
+        List<BarEntry> entriesBar1 = new ArrayList<>();
+        List<BarEntry> entriesBar2 = new ArrayList<>();
+        List<BarEntry> entriesBar3 = new ArrayList<>();
 
-        BarDataSet set = new BarDataSet(entriesBar, "BarDataSet");
+        entriesBar1.add(new BarEntry(0f, 30f));
+        entriesBar1.add(new BarEntry(1f, 80f));
+        entriesBar1.add(new BarEntry(2f, 60f));
+        entriesBar1.add(new BarEntry(3f, 50f));
+        // gap of 2f
+        entriesBar2.add(new BarEntry(5f, 70f));
+        entriesBar2.add(new BarEntry(6f, 60f));
+        entriesBar2.add(new BarEntry(5f, 70f));
+
+        entriesBar3.add(new BarEntry(6f, 60f));
+        entriesBar3.add(new BarEntry(5f, 70f));
+        entriesBar3.add(new BarEntry(6f, 60f));
+
+        BarDataSet set = new BarDataSet(entriesBar1, "Bar");
+        set.setColors(ColorTemplate.COLORFUL_COLORS);
+
         BarData data = new BarData(set);
         data.setBarWidth(0.9f); // set custom bar width
         barChart.setData(data);
@@ -56,7 +69,10 @@ public class ReportActivity extends AppCompatActivity {
         entriesPie.add(new PieEntry(30.8f, "Blue"));
 
         PieDataSet setPie = new PieDataSet(entriesPie, "Election Results");
+        setPie.setColors(ColorTemplate.JOYFUL_COLORS);
         PieData dataPie = new PieData(setPie);
+        pieChart.setUsePercentValues(true);
+        pieChart.getDescription().setEnabled(false);
         pieChart.setData(dataPie);
         pieChart.invalidate(); // refresh
 
